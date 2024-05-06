0ff1d86201e244a734d468c5e1dccfcb246d8430
alexey.kuznetsov <alexey.kuznetsov@latoken.com>
2024-05-06 16:26:35 +0300

    orderId

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -70,24 +70,8 @@ export class OrderService {
         }
     }
 
-    async function generateOrderId(): Promise<string> {
-      const currentYear = new Date().getFullYear().toString();
-      const lastOrder = await this.orderModel.findOne({}, {}, { sort: { 'createdAt': -1 } }).exec();
-      if (lastOrder) {
-        const lastOrderId = lastOrder.orderId;
-        const lastOrderYear = lastOrderId.slice(0, 4);
-        if (lastOrderYear === currentYear) {
-          const lastOrderNumber = parseInt(lastOrderId.slice(4), 10);
-          const nextOrderNumber = lastOrderNumber + 1;
-          const paddedOrderNumber = nextOrderNumber.toString().padStart(6, '0');
-          return currentYear + paddedOrderNumber;
-        }
-      }
-      return currentYear + '000001';
-    }
-
     const order: Order = {
-      orderId: await generateOrderId(),
+      orderId: await this.generateOrderId(),
       phoneNumber: dto.phoneNumber,
       email: dto.email,
       fullName: dto.fullName,
@@ -177,5 +161,21 @@ export class OrderService {
     if (order == null) throw new NotFoundException(`заказ ${orderId} не найден`);
     order.paid = val;
     return order.save();
+  }
+
+  private async generateOrderId(): Promise<string> {
+    const currentYear = new Date().getFullYear().toString();
+    const lastOrder = await this.orderModel.findOne({}, {}, { sort: { 'createdAt': -1 } }).exec();
+    if (lastOrder) {
+      const lastOrderId = lastOrder.orderId;
+      const lastOrderYear = lastOrderId.slice(0, 4);
+      if (lastOrderYear === currentYear) {
+        const lastOrderNumber = parseInt(lastOrderId.slice(4), 10);
+        const nextOrderNumber = lastOrderNumber + 1;
+        const paddedOrderNumber = nextOrderNumber.toString().padStart(6, '0');
+        return currentYear + paddedOrderNumber;
+      }
+    }
+    return currentYear + '000001';
   }
 }
