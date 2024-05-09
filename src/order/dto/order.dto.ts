f20e38e30308e10b6a226ad89875b48647cd8ee1
alexey.kuznetsov <alexey.kuznetsov@latoken.com>
2024-05-10 01:44:57 +0300

    dto

--- src/order/dto/order.dto.ts
+++ src/order/dto/order.dto.ts
@@ -153,7 +153,6 @@ export class CreateOrderDto {
 
   @ApiProperty()
   @IsString()
-  @Matches(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
   phoneNumber: string;
 
   @ApiProperty()
